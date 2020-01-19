48f91bff15fb88b9fa00b664de5aa5ebf0423687
arojit007 <arojit@thirdeyedata.io>
2020-01-19 15:59:53 +0530

    Reading by Kafka Consumer and Persisting into MySQL

---
+++ tech-mantra/src/main/java/com/example/techmantra/repository/UserActivityRepository.java
@@ -0,0 +1,7 @@
+package com.example.techmantra.repository;
+
+import com.example.techmantra.entity.UserActivity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserActivityRepository extends CrudRepository<UserActivity, Integer> {
+}
---
+++ tech-mantra/src/main/java/com/example/techmantra/service/Consumer.java
@@ -0,0 +1,31 @@
+package com.example.techmantra.service;
+
+import com.example.techmantra.entity.UserActivity;
+import com.example.techmantra.repository.UserActivityRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+    @Autowired
+    UserActivityRepository userActivityRepository;
+
+    @KafkaListener(topics = "test", groupId = "g_1")
+    public void consume(String message){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            UserActivity userActivity = mapper.readValue(message, UserActivity.class);
+            userActivityRepository.save(userActivity);
+            Logger logger = LoggerFactory.getLogger(Consumer.class);
+            logger.info(message + " - Record Inserted");
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
